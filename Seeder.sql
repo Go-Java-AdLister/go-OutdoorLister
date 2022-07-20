8e00e6daaadb089d73e6b49933c1005cc9d88dc5
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-07-20 16:33:20 -0500

    feat: create seeder for tables

---
+++ Seeder.sql
@@ -0,0 +1,9 @@
+USE ODlister_db;
+
+INSERT INTO users(username, email, password)
+    VALUE ('test', 'test@gmail.com', 'password');
+
+INSERT INTO ads (user_id, title, createdOn, field, description)
+    VALUE (1, 'Cagle recreation', '2022-07-20', 'camping', 'Nice area in New Waverly for camping trips'),
+    (1, 'Burroughs Park', '2022-06-15', 'fishing', 'Beautiful stocked pond with playgrounds for the kids');
+
