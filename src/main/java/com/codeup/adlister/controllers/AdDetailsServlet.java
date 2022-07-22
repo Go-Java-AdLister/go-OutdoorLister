6fc5d1368dfe15d1097a6486178a995cf15475f5
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-07-22 09:18:53 -0500

    change location of details servlet

--- src/main/java/com/codeup/adlister/controllers/AdDetailsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdDetailsServlet.java
@@ -10,16 +10,16 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "AdDetailsServlet", value = "/AdDetailsServlet")
+@WebServlet(name = "AdDetailsServlet", value = "/details")
 public class AdDetailsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // get all ads from the db with Dao
+//         get all ads from the db with Dao
 //        Ads adDao = DaoFactory.getAdsDao();
 //        Ad ad = adDao.findById();
 
-//        request.getRequestDispatcher("/WEB-INF/details.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/details.jsp").forward(request, response);
     }
 
     @Override
